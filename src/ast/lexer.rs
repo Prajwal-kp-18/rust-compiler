939e7772393d07d0de8829af0e1ebb5062d3f07e
Prajwal <prajwal.kp.1817@gmail.com>
2025-08-07 14:00:07 +0530

    feat(lexer): complete lexer implementation with tokenization for numbers and operators

---
+++ src/ast/lexer.rs
@@ -0,0 +1,146 @@
+
+#[derive(Debug)]
+pub enum TokenKind {
+    Number(i64),
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    LeftParen,
+    RightParen,
+    Bad,
+    EOF,
+    Whitespace,
+    // Identifier,
+    // Number,
+    // String,
+    // Keyword,
+    // Operator,
+    // Punctuation,
+    // Whitespace,
+    // Comment,
+    // Error,
+}  
+
+#[derive(Debug)]
+pub struct TextSpan {
+    pub start: usize,
+    pub end: usize,
+    pub literal: String,
+}
+
+impl TextSpan {
+    pub fn new(start: usize, end: usize, literal:String) -> Self {
+        Self { start, end, literal }
+    }
+
+    pub fn length(&self) -> usize {
+        self.end - self.start
+    }
+}
+
+#[derive(Debug)]
+pub struct Token {
+     pub kind: TokenKind,
+     pub span: TextSpan,
+} 
+
+impl Token {
+    pub fn new(kind: TokenKind, span: TextSpan) -> Self {
+        Self { kind, span }
+    }
+}
+
+pub struct Lexer<'o> {
+    pub input: &'o str,
+    pub current_pos: usize,
+}
+
+impl <'o> Lexer<'o> {
+    pub fn new(input: &'o str) -> Self {
+        Self {
+            input,
+            current_pos: 0,
+        }
+    }
+
+    pub fn next_token(&mut self) -> Option<Token> {
+        if self.current_pos == self.input.len() {
+            self.current_pos += 1;
+            return Some(Token::new(
+                TokenKind::EOF,
+                // doubt
+                TextSpan::new(0,0,'\u{0000}'.to_string())
+            ))
+        }
+        let c: Option<char> = self.current_char();
+        return c.map(|c: char| {
+            let start = self.current_pos;
+            let mut kind = TokenKind::Bad;
+
+            if Self::is_number_start(&c) {
+                let number: i64 = self.consume_number();
+                kind = TokenKind::Number(number);
+            } else if Self::is_whitespace(&c) {
+                self.consume();
+                kind = TokenKind::Whitespace;
+            } else {
+                kind = self.consume_punctuation();
+            }
+
+            let end = self.current_pos;
+            let literal = self.input[start..end].to_string();
+            let span = TextSpan::new(start, end, literal);
+            Token::new(kind, span)
+        });
+    }
+
+    pub fn is_whitespace(c :&char) -> bool {
+        c.is_whitespace() 
+    }
+
+    pub fn consume_punctuation(&mut self) -> TokenKind {
+        let c: char = self.consume().unwrap();
+        match c {
+            '+' => TokenKind::Plus,
+            '-' => TokenKind::Minus,
+            '*' => TokenKind::Asterisk,
+            '/' => TokenKind::Slash,
+            '(' => TokenKind::LeftParen,
+            ')' => TokenKind::RightParen,
+            _ => TokenKind::Bad,
+        }
+    }
+
+    pub fn is_number_start(c: &char) -> bool {
+        c.is_digit(10)
+    }
+
+    pub fn current_char(&self) -> Option<char> {
+        self.input.chars().nth(self.current_pos) 
+    }
+
+
+    pub fn consume(&mut self) -> Option<char> {
+        // doubt > or >=
+        if self.current_pos > self.input.len() {
+            return None;
+        }
+        let c: Option<char> = self.current_char();
+        self.current_pos += 1;
+
+        c
+    }
+    
+    pub fn consume_number(&mut self) -> i64 {
+        let mut number :i64= 0;
+        while let Some(c) = self.current_char() {
+            if !c.is_digit(10) {
+                break;
+            }
+            self.consume().unwrap();
+            number = number * 10 + c.to_digit(10).unwrap() as i64;
+        }
+        number
+    }
+}
